b4278018954a434fb1f43d0eddf01b63c834a82b
gomahera-mymoota <iceman4u@naver.com>
2025-02-11 18:03:28 +0900

    chord

--- src/main/java/com/zynzza/sb/toy/ToyApplication.java
+++ src/main/java/com/zynzza/sb/toy/ToyApplication.java
@@ -1,13 +1,24 @@
 package com.zynzza.sb.toy;
 
-import org.springframework.boot.SpringApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
+@Slf4j
 @SpringBootApplication
 public class ToyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToyApplication.class, args);
+//		SpringApplication.run(ToyApplication.class, args);
+
+//		var application = new SpringApplication(ToyApplication.class);
+//		application.setBannerMode(Banner.Mode.OFF);
+//		application.run(args);
+
+		var appCtx = new SpringApplicationBuilder(ToyApplication.class)
+				.bannerMode(Banner.Mode.CONSOLE)
+				.run();
 	}
 
 }
